004a73116d3bb0221aece954bf049cb001243b58
kah3vich <nik.b20@mail.ru>
2022-04-01 17:37:23 +0300

    fix: header

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -4,40 +4,42 @@ import versionNumber from "gulp-version-number";
 import pug from "gulp-pug";
 
 export const html = () => {
-	return app.gulp
-		.src(app.path.src.html)
-		.pipe(
-			app.plugins.plumber(
-				app.plugins.notify.onError({
-					title: "HTML",
-					message: "Error: <%= error.message %>",
+	return (
+		app.gulp
+			.src(app.path.src.html)
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: "HTML",
+						message: "Error: <%= error.message %>",
+					})
+				)
+			)
+			.pipe(fileInclude())
+			.pipe(
+				pug({
+					pretty: true,
 				})
 			)
-		)
-		.pipe(fileInclude())
-		.pipe(
-			pug({
-				pretty: true,
-			})
-		)
-		.pipe(app.plugins.replace(/@img\//g, "./img/"))
-		.pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				versionNumber({
-					value: "%DT%",
-					append: {
-						key: "_v",
-						cover: 0,
-						to: ["css", "js"],
-					},
-					output: {
-						file: "./version.json",
-					},
-				})
+			.pipe(app.plugins.replace(/@img\//g, "./img/"))
+			// .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					versionNumber({
+						value: "%DT%",
+						append: {
+							key: "_v",
+							cover: 0,
+							to: ["css", "js"],
+						},
+						output: {
+							file: "./version.json",
+						},
+					})
+				)
 			)
-		)
-		.pipe(app.gulp.dest(app.path.build.html))
-		.pipe(app.plugins.browsersync.stream());
+			.pipe(app.gulp.dest(app.path.build.html))
+			.pipe(app.plugins.browsersync.stream())
+	);
 };
