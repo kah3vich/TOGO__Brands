2e81fa612da8439974ee7de1a5d840b661f85e76
kah3vich <nik.b20@mail.ru>
2022-04-05 18:05:34 +0300

    new: media page index

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
 "use strict";
 
+var swiper = new Swiper(".indexNews", {
+  pagination: {
+    el: ".ind-news__s .swiper-pagination",
+    type: "progressbar"
+  },
+  breakpoints: {
+    499: {
+      slidesPerView: 2,
+      spaceBetween: 50
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    }
+  }
+});
 ;
---
+++ src/js/index/index.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper(".indexNews", {
+	pagination: {
+		el: ".ind-news__s .swiper-pagination",
+		type: "progressbar",
+	},
+	breakpoints: {
+		499: {
+			slidesPerView: 2,
+			spaceBetween: 50,
+		},
+		0: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
+	},
+});
